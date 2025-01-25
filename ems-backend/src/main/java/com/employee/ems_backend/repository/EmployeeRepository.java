50b4d90242d2020474b2bc12c8e096d44a8aef60
Dulan Samarasingha <dulansamarasingha138@gmail.com>
2025-01-25 16:01:59 +0530

    Employee Repository

---
+++ ems-backend/src/main/java/com/employee/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.employee.ems_backend.repository;
+
+import com.employee.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+}
