2733b183447cfd38002549883ff38d4f1d32583c
ThArInDu0621 <lakmal8442tharindu@gmail.com>
2025-01-27 12:55:45 +0530

    Add route and header,footer

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -3,13 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ListEmployeeComponents from './components/ListEmployeeComponents'
+import HeaderComponents from './components/HeaderComponents'
+import FooterComponents from './components/FooterComponents'
+import{BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <ListEmployeeComponents/>
+    <BrowserRouter>
+      <HeaderComponents/>
+        <Routes>
+          {/* // http://localhost:3000 */}
+            <Route path='/' element = { <ListEmployeeComponents/>}></Route>
+            {/* // http://localhost:3000/employees */}
+            <Route path='/employees' element = { <ListEmployeeComponents/>}></Route>
+
+        </Routes>
+      <FooterComponents/>
+      </BrowserRouter>
       
     </>
   )
--- ems-frontend/src/components/ListEmployeeComponents.jsx
+++ ems-frontend/src/components/ListEmployeeComponents.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { listEmployees } from '../services/EmployeeService'
 
-function ListEmployeeComponents() {
+const ListEmployeeComponents = () => {
 
-    const dummyData = [
+    const [employees, setEmployees] = useState([])
+
+    useEffect(() => {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
+    }, [])
+
+   /*const dummyData = [
         {
             "id": 1,
             "firstName": "Tharindu",
@@ -21,11 +32,11 @@ function ListEmployeeComponents() {
             "lastName": "Kumarasingha",
             "email": "chathura@gmail.com"
         }
-    ]
+    ]*/
 
   return (
-    <div className='container'>
-        <h2 className='text-center'>List of Employees</h2>
+    <div className='table-container'>
+        <h2 className='text-center mb-4'>List of Employees</h2>
         <table className='table table-striped table-bordered'>
             <thead>
                 <tr>
@@ -37,7 +48,7 @@ function ListEmployeeComponents() {
             </thead>
             <tbody>
                 {
-                    dummyData.map(employee =>
+                    employees.map(employee =>
                         <tr key={employee.id}>
                             <td>{employee.id}</td>
                             <td>{employee.firstName}</td>
